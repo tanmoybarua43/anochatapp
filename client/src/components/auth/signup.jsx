da5ddc80eb29ccc4d8b4899f41cb28df3dcd43e6
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2022-06-08 17:55:34 +0600

    updated

--- client/src/components/auth/signup.jsx
+++ client/src/components/auth/signup.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import image from '.././image'
+import axios from 'axios'
 
-const signup = () => {
+const Signup = () => {
+
+    const [fName, setfName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cPassword, setCPassword] = useState('');
+
+
+        const SignUpUser = async(e) => {
+            e.preventDefault();
+            console.log(fName,email,password,cPassword);
+            try {
+                const response = await axios.post('http://localhost:8080/create',JSON.stringify({fName,email,password,cPassword}),
+                    {
+                        headers: {'Content-Type': 'application/json'},
+                        withCredentials: true
+                    }
+                );
+
+            } catch (error) {
+                if (error) {
+                    console.log('error');
+                }
+            }
+        }
 
   return (
     <section className="signup">
@@ -12,19 +37,19 @@ const signup = () => {
                         <form method="POST" className="register-form" id="register-form">
                             <div className="form-group">
                                 <label htmlFor="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder="Your Name"/>
+                                <input type="text" name="name" id="name" placeholder="Your Name" onChange={(e)=>{setfName(e.target.value)}} value={fName}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="email"><i className="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                                <input type="email" name="email" id="email" placeholder="Your Email" onChange={(e)=>{setEmail(e.target.value)}} value={email} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="pass"><i className="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                                <input type="password" name="pass" id="pass" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} value={password}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="re-pass"><i className="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" onChange={(e)=>{setCPassword(e.target.value)}} value={cPassword} />
                             </div>
                             <div className="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" className="agree-term" />
@@ -45,4 +70,4 @@ const signup = () => {
  )
 }
 
-export default signup
+export default Signup
